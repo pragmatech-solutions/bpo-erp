83b6e5ed3af2dce0f2de3b5b83337bf69eb58be7
Burhan Ahmed <burhanahmedsatti.gmail.com>
2026-06-04 02:50:21 +0500

    fix(leads): add error property to UpdateLeadResponse type

--- leads/backend/update-lead/update-lead.type.ts
+++ leads/backend/update-lead/update-lead.type.ts
@@ -3,6 +3,7 @@ import { UpdateLeadInput } from './update-lead.input-schema';
 export type UpdateLeadResponse = {
 	success: boolean;
 	message?: string;
+	error?: string;
 };
 
 export type { UpdateLeadInput };
